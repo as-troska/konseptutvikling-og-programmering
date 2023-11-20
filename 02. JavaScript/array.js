386b0d188bb9782093a47bfd220bfe135c43967e
Trond Sneås Skauge <troska@asvg.no>
2023-11-20 09:06:30 +0100

    Flere oppgaver

--- 02. JavaScript/array.js
+++ 02. JavaScript/array.js
@@ -20,3 +20,54 @@ for (let tallet of tall) {
         console.log(tallet)
     }
 }
+
+
+//Redigere arrayer
+
+//Lager et tomt array
+let etArray = []
+
+//Legger til elementer i slutten av arrayet
+etArray.push("Hei")
+etArray.push("Hallo")
+
+//Fjerner det siste elementet i arrayet
+etArray.pop()
+
+//Legger til elementer i starten av arrayet
+etArray.unshift("Bonjour")
+
+//Fjerner det første elementet i arrayet
+etArray.shift()
+
+//Endrer et gitt element i arrayet
+etArray[0] = "Bonjour"
+
+//Logger innholdet i arrayet
+console.log(etArray)
+
+//Lager et array med navn på byer
+let byer = ["Oslo", "Bergen", "Trondheim", "Stavanger", "Kristiansand", "Tromsø", "Bergen"]
+
+//Finner indexen til første instans av Bergen
+console.log(byer.indexOf("Bergen"))
+
+//Finner indexen til Alta, som ikke eksistere i arrayet. Da returneres -1
+console.log(byer.indexOf("Alta"))
+
+//Finner indexen til siste instans av Bergen
+console.log(byer.lastIndexOf("Bergen"))
+
+//Finner indexen til første instans av Bergen etter index 3
+console.log(byer.indexOf("Bergen", 3))
+
+//Fjerner Oslo fra arrayet (index 0)
+byer.splice(0, 1)
+console.log(byer)
+
+//Går gjennom arrayet og fjerner alle instanser av Bergen
+for (let i = 0; i < byer.length; i++) {
+    if (byer[i] === "Bergen") {
+        byer.splice(i, 1)
+    }
+}
